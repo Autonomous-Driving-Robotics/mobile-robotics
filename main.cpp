df099c949dd3f82db647415045d025076cc59d0b
Dikshant Gupta <dikshant2210@gmail.com>
2024-01-24 23:49:17 +0000

    2d matrix data structures

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ int main()
     types::FixedSizeVector<float, 2> A;
     std::cout << A.Size() << std::endl;
 
-    types::SquareMatrix<float, 2> B;
-    std::cout << B.Size();
+    stdmath::matrix::ZerosMatrix<float> B(2, 2);
+    std::cout << "[[" << B(0, 0) << ", " << B(0, 1) << "],\n[" << B(1, 0) << ", " << B(1, 1)
+              << "]]\n";
+    auto shape{B.size()};
     return 0;
 }
--- src/types/matrix.hpp
+++ src/types/matrix.hpp
@@ -1,43 +1,117 @@
 #ifndef TYPES_MATRIX_HPP
 #define TYPES_MATRIX_HPP
 
-#include "iostream"
+#include "memory"
 #include "tuple"
 #include "vector"
 
-namespace types
+namespace stdmath
 {
 
-struct matrix_size
+namespace matrix
 {
-    int rows{0};
-    int columns{0};
 
-    friend std::ostream& operator<<(std::ostream& os, const matrix_size& size)
-    {
-        os << "(" << size.rows << ", " << size.columns << ")\n";
-        return os;
-    }
-};
-
-template <typename T, int rows>
-class SquareMatrix
+template <typename T>
+class Matrix2D
 {
   private:
-    std::vector<T> matrix;
+    T* data;
+    size_t rows;
+    size_t cols;
 
   public:
-    SquareMatrix()
+    Matrix2D(size_t rows, size_t cols) : rows(rows), cols(cols), data(new T[rows * cols])
     {
-        matrix.resize(rows * rows);
+        for (size_t t = 0; t < rows * cols; t++)
+        {
+            data[t] = 0.0;
+        }
     }
-
-    matrix_size Size()
+    ~Matrix2D()
     {
-        return matrix_size{rows, rows};
+        delete[] data;
+    }
+    inline T* getData()
+    {
+        return data;
+    }
+    inline size_t& getRows()
+    {
+        return rows;
+    }
+    inline size_t& getCols()
+    {
+        return cols;
+    }
+    inline T& operator()(size_t row, size_t col)
+    {
+        return data[row * cols + col];
+    }
+    inline T& operator()(size_t row, size_t col) const
+    {
+        return data[row * cols + col];
+    }
+    std::tuple<size_t, size_t> size()
+    {
+        return std::make_tuple(rows, cols);
     }
 };
 
-}  // namespace types
+template <typename T>
+class ZerosMatrix : public Matrix2D<T>
+{
+  public:
+    ZerosMatrix(size_t rows, size_t cols) : Matrix2D<T>(rows, cols) {}
+    ~ZerosMatrix() {}
+};
+
+template <typename T>
+class OnesMatrix : public Matrix2D<T>
+{
+  public:
+    OnesMatrix(size_t rows, size_t cols) : Matrix2D<T>(rows, cols)
+    {
+        T* data = this->getData();
+        for (size_t t = 0; t < rows * cols; t++)
+        {
+            data[t] = 1.0;
+        }
+    }
+    ~OnesMatrix() {}
+};
+
+template <typename T>
+class SquareMatrix : public Matrix2D<T>
+{
+  public:
+    SquareMatrix(size_t rows) : Matrix2D<T>(rows, rows)
+    {
+        T* data = this->getData();
+        for (size_t t = 0; t < rows * rows; t++)
+        {
+            data[t] = 0.0;
+        }
+    }
+    ~SquareMatrix() {}
+};
+
+template <typename T>
+class IdentityMatrix : public Matrix2D<T>
+{
+  public:
+    IdentityMatrix(size_t rows) : Matrix2D<T>(rows, rows)
+    {
+        T* data = this->getData();
+        for (size_t t = 0; t < rows; t++)
+        {
+            data[t * rows + t] = 1.0;
+        }
+    }
+    ~IdentityMatrix() {}
+};
+
+}  // namespace matrix
+
+}  // namespace stdmath
 
 #endif
