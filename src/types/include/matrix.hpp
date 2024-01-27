5dd51f7f30317f3baaa16ac8670fd32b309ea87b
Dikshant Gupta <dikshant2210@gmail.com>
2024-01-27 23:55:24 +0000

    add standar matrix header

--- src/types/include/matrix.hpp
+++ src/types/include/matrix.hpp
@@ -1,6 +1,7 @@
 #ifndef TYPES_MATRIX_HPP
 #define TYPES_MATRIX_HPP
 
+#include "iostream"
 #include "memory"
 #include "tuple"
 #include "vector"
@@ -11,103 +12,22 @@ namespace stdmath
 namespace matrix
 {
 
-template <typename T>
-class Matrix2D
+template <typename T, size_t numberOfRows, size_t numberOfColumns, bool rowMajor = true>
+class MatrixXX
 {
-  private:
+  protected:
     T* data;
-    size_t rows;
-    size_t cols;
 
   public:
-    Matrix2D(size_t rows, size_t cols) : rows(rows), cols(cols), data(new T[rows * cols])
-    {
-        for (size_t t = 0; t < rows * cols; t++)
-        {
-            data[t] = 0.0;
-        }
-    }
-    ~Matrix2D()
-    {
-        delete[] data;
-    }
-    inline T* getData()
-    {
-        return data;
-    }
-    inline size_t& getRows()
-    {
-        return rows;
-    }
-    inline size_t& getCols()
-    {
-        return cols;
-    }
-    inline T& operator()(size_t row, size_t col)
-    {
-        return data[row * cols + col];
-    }
-    inline T& operator()(size_t row, size_t col) const
-    {
-        return data[row * cols + col];
-    }
-    std::tuple<size_t, size_t> size()
-    {
-        return std::make_tuple(rows, cols);
-    }
-};
-
-template <typename T>
-class ZerosMatrix : public Matrix2D<T>
-{
-  public:
-    ZerosMatrix(size_t rows, size_t cols) : Matrix2D<T>(rows, cols) {}
-    ~ZerosMatrix() {}
-};
-
-template <typename T>
-class OnesMatrix : public Matrix2D<T>
-{
-  public:
-    OnesMatrix(size_t rows, size_t cols) : Matrix2D<T>(rows, cols)
-    {
-        T* data = this->getData();
-        for (size_t t = 0; t < rows * cols; t++)
-        {
-            data[t] = 1.0;
-        }
-    }
-    ~OnesMatrix() {}
-};
-
-template <typename T>
-class SquareMatrix : public Matrix2D<T>
-{
-  public:
-    SquareMatrix(size_t rows) : Matrix2D<T>(rows, rows)
-    {
-        T* data = this->getData();
-        for (size_t t = 0; t < rows * rows; t++)
-        {
-            data[t] = 0.0;
-        }
-    }
-    ~SquareMatrix() {}
-};
-
-template <typename T>
-class IdentityMatrix : public Matrix2D<T>
-{
-  public:
-    IdentityMatrix(size_t rows) : Matrix2D<T>(rows, rows)
-    {
-        T* data = this->getData();
-        for (size_t t = 0; t < rows; t++)
-        {
-            data[t * rows + t] = 1.0;
-        }
-    }
-    ~IdentityMatrix() {}
+    MatrixXX();
+    ~MatrixXX();
+    inline std::tuple<size_t, size_t> size() const;
+    inline T& operator()(size_t row, size_t col);
+    inline T& operator()(size_t row, size_t col) const;
+    inline size_t rows() const;
+    inline size_t cols() const;
+    void resize();
+    std::ostream& operator<<(std::ostream os);
 };
 
 }  // namespace matrix
